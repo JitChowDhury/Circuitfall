105f84339b85e313ae26e049282906df8c84b289
JitChowDhury <jitchowdhury8717@gmail.com>
2025-05-29 22:14:37 +0530

    Enemy Rotation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -7,6 +7,7 @@ public class Enemy : MonoBehaviour
     private NavMeshAgent agent;
 
     [SerializeField] private Transform[] waypoints;
+    [SerializeField] private float turnSpeed = 10f;
     private int currentIndex;
     
 
@@ -24,12 +25,25 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        FaceTarget(agent.steeringTarget);
+            //checks if the agent is close to the current target point
         if (agent.remainingDistance < 0.5)
         {
+            //set the destination to the next waypoint
             agent.SetDestination(GetNextWayPoints());
         }
     }
 
+    private void FaceTarget(Vector3 newTarget)
+    {
+        //calculate the distance from current position to the new target
+        Vector3 direction = newTarget - transform.position;
+        direction.y = 0;//removes vertical component
+        //create a rotation that points the forward vector up the calculated direction
+        Quaternion targetRotation=Quaternion.LookRotation(direction);
+        //smoothly rotate from the current rotation to the target rotation at the defined speed
+        transform.rotation=Quaternion.Lerp(transform.rotation,targetRotation,turnSpeed*Time.deltaTime);
+    }
     private Vector3 GetNextWayPoints()
     {
         if (currentIndex >= waypoints.Length)
