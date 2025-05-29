8931f5ab2deaba8daac35721ae5a577aa5c19959
JitChowDhury <jitchowdhury8717@gmail.com>
2025-05-29 20:36:22 +0530

    Enemy Movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,7 +6,9 @@ public class Enemy : MonoBehaviour
 {
     private NavMeshAgent agent;
 
-    [SerializeField] private Transform waypoint;
+    [SerializeField] private Transform[] waypoints;
+    private int currentIndex;
+    
 
     private void Awake()
     {
@@ -16,12 +18,27 @@ public class Enemy : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        agent.SetDestination(waypoint.position);
+        //agent.SetDestination(waypoint.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (agent.remainingDistance < 0.5)
+        {
+            agent.SetDestination(GetNextWayPoints());
+        }
+    }
+
+    private Vector3 GetNextWayPoints()
+    {
+        if (currentIndex >= waypoints.Length)
+        {
+            return transform.position;
+        } 
+        Vector3 nextDestination = waypoints[currentIndex].position;
+
+        currentIndex++;
+        return nextDestination;
     }
 }
