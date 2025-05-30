baf6df79379df81d22da97d6ff2d08a5c182b381
JitChowDhury <jitchowdhury8717@gmail.com>
2025-05-30 14:40:22 +0530

    Respawn enemies

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,28 @@
+using System;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    [SerializeField] private Transform respawn;
+    [SerializeField] float spawnCooldown;
+    private float spawnTimer;
+
+    [Header("Enemy Prefabs")] 
+    [SerializeField] private GameObject basicEnemy;
+
+    private void Update()
+    {
+        spawnTimer -= Time.deltaTime;
+        if (spawnTimer <= 0)
+        {
+            CreateEnemy();
+            spawnTimer = spawnCooldown;
+        }
+       
+    }
+
+    private void CreateEnemy()
+    {
+        GameObject newEnemy=Instantiate(basicEnemy, respawn.position, Quaternion.identity);
+    }
+}
