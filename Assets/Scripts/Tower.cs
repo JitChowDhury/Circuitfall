1f454773525ae5b38d6de44f67f3fc0631a1a5ad
JitChowDhury <jitchowdhury8717@gmail.com>
2025-05-30 21:33:37 +0530

    Target Random Enemy Within Attack Range

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Tower : MonoBehaviour
@@ -9,22 +9,50 @@ public class Tower : MonoBehaviour
      [SerializeField] private Transform towerHead;
 
      [SerializeField] private float rotationSpeed;
-
+     [SerializeField] private float attackRange = 2.2f;
+     [SerializeField] private LayerMask whatIsEnemy;
      private void Update()
      {
+          if (currentEnemy == null)
+          {
+              currentEnemy= FindRandomEnemiesWithinTarget();
+               return;
+          }
+          if (Vector3.Distance(currentEnemy.position, transform.position) > attackRange) currentEnemy = null;
           RotateTowardsEnemy();
      }
 
      private void RotateTowardsEnemy()
      {
+          if(currentEnemy==null)return;
           //calculate the vector direction from the tower's head to the current enemy
           Vector3 directionToEnemy = currentEnemy.position - towerHead.position;
           //create a Quaternion for the rotation towards the direction of enemy
           Quaternion lookRotation = Quaternion.LookRotation(directionToEnemy);
-          //smoothly rotates the towerhead to the enemy
+          //smoothly rotates the tower head to the enemy
           Vector3 rotation = Quaternion.Lerp(towerHead.rotation, lookRotation, rotationSpeed * Time.deltaTime)
                .eulerAngles;
           //converts euler to quaternion for unity to understand easily
           towerHead.rotation=Quaternion.Euler(rotation);
+     }
+
+     private Transform FindRandomEnemiesWithinTarget()
+     {
+          List<Transform> possibleTargets = new List<Transform>();
+          Collider[] enemiesAround = Physics.OverlapSphere(transform.position, attackRange,whatIsEnemy);
+          foreach (Collider enemy in enemiesAround)
+          {
+               possibleTargets.Add(enemy.transform);
+          }
+
+          int randomIndex = Random.Range(0, possibleTargets.Count);
+          if (possibleTargets.Count <= 0) return null;
+          return possibleTargets[randomIndex];
+
+     }
+     
+     private void OnDrawGizmos()
+     {
+          Gizmos.DrawWireSphere(transform.position,attackRange);
      }
 }
