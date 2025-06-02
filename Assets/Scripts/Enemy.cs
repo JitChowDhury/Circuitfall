e507e98d2e08e72b84d4380f560f7cb8ba0aed32
JitChowDhury <jitchowdhury8717@gmail.com>
2025-06-02 20:46:53 +0530

    Most Advanced Target

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,20 +1,18 @@
-using System;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Enemy : MonoBehaviour,IDamagable
+public class Enemy : MonoBehaviour, IDamagable
 {
-    private NavMeshAgent agent;
-
     public int healthPoints = 4;
-    [Header("Movement")]
-    [SerializeField] private Transform[] waypoints;
+
+    [Header("Movement")] [SerializeField] private Transform[] waypoints;
+
     [SerializeField] private float turnSpeed = 10f;
+    private NavMeshAgent agent;
     private int currentIndex;
 
     private float totalDistance;
 
-    
 
     private void Awake()
     {
@@ -24,31 +22,39 @@ public class Enemy : MonoBehaviour,IDamagable
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
         waypoints = FindFirstObjectByType<WayPointManager>().GetWayPoints();
         CollectTotalDistance();
     }
-    
+
 
     // Update is called once per frame
-    void Update()
-    { 
+    private void Update()
+    {
         FaceTarget(agent.steeringTarget);
-            //checks if the agent is close to the current target point
+        //checks if the agent is close to the current target point
         if (agent.remainingDistance < 0.5)
-        {
             //set the destination to the next waypoint
             agent.SetDestination(GetNextWayPoints());
-        }
     }
 
-    public float DistanceToFinishLine() => totalDistance + agent.remainingDistance;
+    public void TakeDamage(int damage)
+    {
+        healthPoints -= damage;
+        if (healthPoints <= 0) Destroy(gameObject);
+    }
+
+    public float DistanceToFinishLine()
+    {
+        return totalDistance + agent.remainingDistance;
+    }
+
     private void CollectTotalDistance()
     {
-        for (int i = 0; i < waypoints.Length - 1; i++)
+        for (var i = 0; i < waypoints.Length - 1; i++)
         {
-            float distance = Vector3.Distance(waypoints[i].position, waypoints[i + 1].position);
+            var distance = Vector3.Distance(waypoints[i].position, waypoints[i + 1].position);
             totalDistance += distance;
         }
     }
@@ -56,39 +62,33 @@ public class Enemy : MonoBehaviour,IDamagable
     private void FaceTarget(Vector3 newTarget)
     {
         //calculate the distance from current position to the new target
-        Vector3 direction = newTarget - transform.position;
-        direction.y = 0;//removes vertical component
+        var direction = newTarget - transform.position;
+        direction.y = 0; //removes vertical component
         //create a rotation that points the forward vector up the calculated direction
-        Quaternion targetRotation=Quaternion.LookRotation(direction);
+        var targetRotation = Quaternion.LookRotation(direction);
         //smoothly rotate from the current rotation to the target rotation at the defined speed
-        transform.rotation=Quaternion.Lerp(transform.rotation,targetRotation,turnSpeed*Time.deltaTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
     }
+
     private Vector3 GetNextWayPoints()
     {
         //check if the waypoint index is beyonf the last waypoint
         if (currentIndex >= waypoints.Length)
-        {
-        //if true . return the agent's current position,effectively stopping it
+            //if true . return the agent's current position,effectively stopping it
             return transform.position;
-        } 
         //get the current target point from the waypoints array
-        Vector3 nextDestination = waypoints[currentIndex].position;
-//if this is not the first waypoint , calculate the distance from the previous waypoint
+        var nextDestination = waypoints[currentIndex].position;
+        //if this is not the first waypoint , calculate the distance from the previous waypoint
         if (currentIndex > 0)
         {
-            float distance = Vector3.Distance(waypoints[currentIndex].position, waypoints[currentIndex - 1].position);
+            var distance = Vector3.Distance(waypoints[currentIndex].position, waypoints[currentIndex - 1].position);
             //substract this distance from the total distance
             totalDistance = totalDistance - distance;
         }
-//increment the waypoint index to move to the next waypoint on the next call
+
+        //increment the waypoint index to move to the next waypoint on the next call
         currentIndex++;
         //return the current target point;
         return nextDestination;
-    }
-
-    public void TakeDamage(int damage)
-    {
-        healthPoints -= damage;
-        if(healthPoints<=0) Destroy(gameObject);
     }
 }
--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,97 +1,121 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class Tower : MonoBehaviour
 {
-     public Transform currentEnemy;
+    public Transform currentEnemy;
 
-     [Header("Tower Setup")] 
-     [SerializeField] protected Transform towerHead;
+    [Header("Tower Setup")] [SerializeField]
+    protected Transform towerHead;
 
-     [SerializeField] protected float rotationSpeed=10f;
-     [SerializeField] protected float attackRange = 2.5f;
-     [SerializeField] protected LayerMask whatIsEnemy;
-     [SerializeField] protected float attackCooldown=1;
+    [SerializeField] protected float rotationSpeed = 10f;
+    [SerializeField] protected float attackRange = 2.5f;
+    [SerializeField] protected LayerMask whatIsEnemy;
+    [SerializeField] protected float attackCooldown = 1;
 
-     private bool canRotate;
-     protected float lastTimeAttacked;
+    private bool canRotate;
+    protected float lastTimeAttacked;
 
-     protected virtual void Awake()
-     {
-     }
+    protected virtual void Awake()
+    {
+    }
 
-     protected virtual void Update()
-     {
-          
-          if (currentEnemy == null)
-          {
-              currentEnemy= FindRandomEnemiesWithinTarget();
-               return;
-          }
-          if(CanAttack()) Attack();
-          if (Vector3.Distance(currentEnemy.position, transform.position) > attackRange) currentEnemy = null;
-          RotateTowardsEnemy();
-     }
+    protected virtual void Update()
+    {
+        if (currentEnemy == null)
+        {
+            currentEnemy = FindRandomEnemiesWithinTarget();
+            return;
+        }
 
-     protected bool CanAttack()
-     {
-          if (Time.time > lastTimeAttacked + attackCooldown)
-          {
-               lastTimeAttacked = Time.time;
-               return true;
-          }
+        if (CanAttack()) Attack();
+        if (Vector3.Distance(currentEnemy.position, transform.position) > attackRange) currentEnemy = null;
+        RotateTowardsEnemy();
+    }
 
-          return false;
-     }
+    protected virtual void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, attackRange);
+    }
 
-     protected virtual void Attack()
-     {
-          Debug.Log("Attack performed at" + Time.time);
-     }
+    protected bool CanAttack()
+    {
+        if (Time.time > lastTimeAttacked + attackCooldown)
+        {
+            lastTimeAttacked = Time.time;
+            return true;
+        }
 
-     public void EnableRotation(bool enable)
-     {
-          canRotate = enable;
-     }
-     protected void RotateTowardsEnemy()
-     {
-          if(canRotate==false)return;
-          if(currentEnemy==null)return;
-          //calculate the vector direction from the tower's head to the current enemy
-          Vector3 directionToEnemy = currentEnemy.position - towerHead.position;
-          //create a Quaternion for the rotation towards the direction of enemy
-          Quaternion lookRotation = Quaternion.LookRotation(directionToEnemy);
-          //smoothly rotates the tower head to the enemy
-          Vector3 rotation = Quaternion.Lerp(towerHead.rotation, lookRotation, rotationSpeed * Time.deltaTime)
-               .eulerAngles;
-          //converts euler to quaternion for unity to understand easily
-          towerHead.rotation=Quaternion.Euler(rotation);
-     }
+        return false;
+    }
 
-     protected Transform FindRandomEnemiesWithinTarget()
-     {
-          List<Transform> possibleTargets = new List<Transform>();
-          Collider[] enemiesAround = Physics.OverlapSphere(transform.position, attackRange,whatIsEnemy);
-          foreach (Collider enemy in enemiesAround)
-          {
-               possibleTargets.Add(enemy.transform);
-          }
+    protected virtual void Attack()
+    {
+        Debug.Log("Attack performed at" + Time.time);
+    }
 
-          int randomIndex = Random.Range(0, possibleTargets.Count);
-          if (possibleTargets.Count <= 0) return null;
-          return possibleTargets[randomIndex];
+    public void EnableRotation(bool enable)
+    {
+        canRotate = enable;
+    }
 
-     }
-     
-     protected virtual void OnDrawGizmos()
-     {
-          Gizmos.DrawWireSphere(transform.position,attackRange);
-     }
+    protected void RotateTowardsEnemy()
+    {
+        if (canRotate == false) return;
+        if (currentEnemy == null) return;
+        //calculate the vector direction from the tower's head to the current enemy
+        var directionToEnemy = currentEnemy.position - towerHead.position;
+        //create a Quaternion for the rotation towards the direction of enemy
+        var lookRotation = Quaternion.LookRotation(directionToEnemy);
+        //smoothly rotates the tower head to the enemy
+        var rotation = Quaternion.Lerp(towerHead.rotation, lookRotation, rotationSpeed * Time.deltaTime)
+            .eulerAngles;
+        //converts euler to quaternion for unity to understand easily
+        towerHead.rotation = Quaternion.Euler(rotation);
+    }
 
-     protected Vector3 DirectionToEnemyFrom(Transform startPos)
-     {
-          return (currentEnemy.position - startPos.position).normalized;
-     }
+    protected Transform FindRandomEnemiesWithinTarget()
+    {
+        List<Enemy> possibleTargets = new List<Enemy>();
+        Collider[] enemiesAround = Physics.OverlapSphere(transform.position, attackRange, whatIsEnemy);
+        foreach (Collider enemy in enemiesAround)
+        {
+            Enemy newEnemy = enemy.GetComponent<Enemy>();
+            possibleTargets.Add(newEnemy);
+        }
+
+
+        Enemy newTarget = GetMostAdvancedenemy(possibleTargets);
+        if (newTarget != null)
+        {
+            return newTarget.transform;
+        }
+
+        return null;
+
+    }
+
+    private Enemy GetMostAdvancedenemy(List<Enemy> possibleTargets)
+    {
+        Enemy mostAdvancedEnemy = null;
+        float minRemainingdistance = float.MaxValue;
+
+        foreach (Enemy enemy in possibleTargets)
+        {
+            float remainingDistance = enemy.DistanceToFinishLine();
+            if (remainingDistance < minRemainingdistance)
+            {
+                minRemainingdistance = remainingDistance;
+                mostAdvancedEnemy = enemy;
+            }
+        }
+
+        return mostAdvancedEnemy;
+    }
+
+    protected Vector3 DirectionToEnemyFrom(Transform startPos)
+    {
+        return (currentEnemy.position - startPos.position).normalized;
+    }
 }
