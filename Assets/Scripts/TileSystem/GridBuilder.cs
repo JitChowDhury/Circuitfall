c53a3a776093ca818f75a70f7291cd782e4ce0c7
JitChowDhury <jitchowdhury8717@gmail.com>
2025-06-09 14:47:01 +0530

    Grid Builder

---
+++ Assets/Scripts/TileSystem/GridBuilder.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridBuilder : MonoBehaviour
+{
+    [SerializeField] private GameObject mainPrefab;
+
+    [SerializeField] private int gridLength = 10;
+    [SerializeField] private int gridWidth = 10;
+
+    [SerializeField] private List<GameObject> createdTiles = new List<GameObject>();
+
+    [ContextMenu("Build Grid")]
+    private void BuildGrid()
+    {
+        createdTiles = new List<GameObject>();
+
+        for (int x = 0; x < gridLength; x++)
+        {
+            for (int z = 0; z < gridWidth; z++)
+            {
+            CreateTile(x,z);
+                
+            }
+        }
+    }
+
+    [ContextMenu("Clear grid")]
+    private void ClearGrid()
+    {
+        foreach (GameObject tile in createdTiles)
+        {
+            DestroyImmediate(tile);
+        }
+
+        createdTiles.Clear();
+    }
+
+    private void CreateTile(float xPos,float zPos)
+    {
+        Vector3 newPos = new Vector3(xPos, 0 , zPos);
+        GameObject newTile = Instantiate(mainPrefab, newPos, Quaternion.identity, transform);
+
+        createdTiles.Add(newTile);
+    }
+}
