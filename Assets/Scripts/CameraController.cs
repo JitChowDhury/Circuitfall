0513cd008d14bdffb3063fcffbc8f172c43a52a7
JitChowDhury <jitchowdhury8717@gmail.com>
2025-06-16 13:08:58 +0530

    Camera Rotation

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,16 +3,54 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    [Header(("Rotation Details"))]
+    [SerializeField] private Transform focusPoint;
+    [SerializeField] private float rotationSpeed = 200f;
+    [Space] 
+    private float pitch;
+    [SerializeField]private float minPitch = 5f;
+    [SerializeField]private float maxPitch = 85f;
+    [SerializeField] private float maxFocusPointDistance = 15;
+    
+    
+    
     [SerializeField] private float movementSpeed = 120;
     private float smoothTime = .1f;
-    private Vector3 movementVelocity=Vector3.zero;
+    private Vector3 movementVelocity = Vector3.zero;
 
     private void Update()
     {
         HandleMovement();
+        HandleRotation();
+
+        focusPoint.position = transform.position + (transform.forward * getFocusPointDistance());
     }
 
-    private void HandleMovement()
+    private float getFocusPointDistance()
+    {
+        if (Physics.Raycast(transform.position, transform.forward,out RaycastHit hit, maxFocusPointDistance))
+        {
+            return hit.distance;
+        }
+
+        return maxFocusPointDistance;
+    }
+    private void HandleRotation()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            float horizontalRotation = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+            float verticalRotation = Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
+
+
+            pitch = Mathf.Clamp(pitch - verticalRotation, minPitch, maxPitch);
+            transform.RotateAround(focusPoint.position,Vector3.up,horizontalRotation);
+            transform.RotateAround(focusPoint.position,transform.right,pitch-transform.eulerAngles.x);
+            transform.LookAt(focusPoint);
+        }
+    }
+
+private void HandleMovement()
     {
         Vector3 targetPosition = transform.position;
 
